3ab146af3ce5c34a21395aeaf1a4e392efd1236b
Bojan Cup <bojan@fgol.co.uk>
2016-05-20 16:05:38 +0100

    Added plugin and test

---
+++ Unity3dPlugin/Assets/Example/ExampleScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleScene : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		//	Load
+		Debug.Log("Loading MALI plugin");
+		Plugins.MALI.MaliProfilerPlugin.Load();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Unity3dPlugin/Assets/Plugins/Android/MaliProfilerPlugin/MaliProfilerPlugin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Plugins.MALI
+{
+	/// <summary>
+	/// Mali profiler plugin class
+	/// </summary>
+	public static class MaliProfilerPlugin
+	{
+		/// <summary>
+		/// Loads Mali Profiler Library 
+		/// </summary>
+		public static void Load()
+		{
+#if UNITY_ANDROID
+			using (var pluginClass = new AndroidJavaClass("com.mihailogazda.maliprofileplugin.MaliPlugin"))
+			{
+				pluginClass.CallStatic("Load");
+			}
+#else
+			Debug.Log("Cannot load Mali profiler on non-android platforms.");
+#endif
+		}
+	}
+
+}
